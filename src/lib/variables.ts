dac7d42a94c69529de044589bc66709b4abd583e
kirkbyers <kirklbyers@gmail.com>
2025-06-25 23:58:33 -0500

    Initial commit

---
+++ src/lib/variables.ts
@@ -0,0 +1,43 @@
+// Set any item to undefined to remove it from the site or to use the default value
+
+export const GLOBAL = {
+  // Site metadata
+  username: "KirkByers",
+  rootUrl: "https://blog.heartened.coffee",
+  shortDescription: "A SWE Shouting Into The Void",
+  longDescription: "Who really starts a blog in 2025? Do you really need to read this strangers thoughts on the state of Software? Yet here you are.",
+  
+  // Social media links
+  githubProfile: "https://github.com/kirkbyers",
+  twitterProfile: null,
+  linkedinProfile: "https://www.linkedin.com/in/kirklbyers/",
+  
+  // Common text names used throughout the site
+  articlesName: "Articles",
+  projectsName: "Projects",
+  viewAll: "View All",
+  
+  // Common descriptions used throughout the site
+  noArticles: "No featured articles yet.",
+  noProjects: "No featured projects yet.",
+
+  // Blog metadata
+  blogTitle: "My Thoughts & Takes",
+  blogShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
+  blogLongDescription: "Web development, tech trends, and the occasional programming mishap.",
+
+  // Project metadata
+  projectTitle: "Projects and Code",
+  projectShortDescription: "Some projects and code I remebered to write about",
+  projectLongDescription: "My personal projects are all over the place. Full-stack and embeded applications. Some tools.",
+
+  // Profile image
+  profileImage: "kirk.jpg",
+
+  // Menu items
+  menu: {
+    home: "/",
+    projects: "/projects",
+    blog: "/blog",
+  }
+};
